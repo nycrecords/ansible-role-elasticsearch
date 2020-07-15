bbb0b487c3b6efbd0108e4e620a9f9843048502f
johnyu95 <jyu@records.nyc.gov>
2020-07-15 10:57:44 -0400

    Updated tests for elasticsearch

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -7,6 +7,5 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-def test_fits_install_dir_exists(host):
-    assert host.socket("tcp://127.0.0.1:9200").is_listening
-    assert not host.socket("tcp://0.0.0.0:9200").is_listening
+def test_elasticsearch_install(host):
+    assert host.service("elasticsearch").is_running
