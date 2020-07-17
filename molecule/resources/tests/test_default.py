e5c2360bf37787592887131d70ae29917caf5462
johnyu95 <jyu@records.nyc.gov>
2020-07-17 15:56:08 -0400

    Updated ssl and configuration tasks

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -7,5 +7,15 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-def test_elasticsearch_install(host):
-    assert host.service("elasticsearch").is_running
+def test_service_is_running_and_enabled(host):
+    elasticsearch = host.service("elasticsearch")
+
+    assert elasticsearch.is_running
+    assert elasticsearch.is_enabled
+
+
+def test_package_is_installed(host):
+    elasticsearch = host.package('elasticsearch')
+
+    assert elasticsearch.is_installed
+    assert elasticsearch.version.startswith('7')
